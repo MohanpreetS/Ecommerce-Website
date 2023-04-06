d319f0333c51e0b60c04e2bb7d461c86fcf550ff
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2023-04-06 13:09:04 -0400

    create category card

---
+++ src/app/_components/Categories/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Link from 'next/link'
+
+import { Category } from '../../../payload/payload-types'
+import CategoryCard from './CategoryCard'
+
+import classes from './index.module.scss'
+
+const Categories = ({ categories }: { categories: Category[] }) => {
+  return (
+    <section className={classes.container}>
+     
+    </section>
+  )
+}
+
+export default Categories
