5542bddbc2dd0e9b575d033141356a3732608b60
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2023-04-09 13:09:04 -0400

    filter provider setup

---
+++ src/app/_providers/Filter/index.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import { createContext, ReactNode, SetStateAction, useContext, useState } from 'react'
+
+interface IContextType {
+  categoryFilters: string[]
+  setCategoryFilters: React.Dispatch<SetStateAction<string[]>>
+  sort: string
+  setSort: React.Dispatch<SetStateAction<string>>
+}
+
+export const INITIAL_FILTER_DATA = {
+  categoryFilters: [],
+  setCategoryFilters: () => [],
+  sort: '',
+  setSort: () => '',
+}
+
+const FilterContext = createContext<IContextType>(INITIAL_FILTER_DATA)
+
+
+
+export const useFilter = () => useContext(FilterContext)
