71058226ac0ea6c81f2d70d6a2611909b687a125
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2023-05-02 13:09:04 -0400

    setup interval function

--- src/app/_components/Promotion/index.tsx
+++ src/app/_components/Promotion/index.tsx
@@ -11,6 +11,27 @@ const Promotion = () => {
     seconds: 0,
   })
 
+  const targetDate = new Date()
+  targetDate.setDate(targetDate.getDate() + 3)
+
+  useEffect(() => {
+    const timerInterval = setInterval(() => {
+      const currentTime = new Date()
+      const timeDifference = Math.max(Number(targetDate) - Number(currentTime), 0)
+
+      const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24))
+      const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+      const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60))
+      const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000)
+
+     
+    }, 1000)
+
+    return () => {
+      clearInterval(timerInterval) // Cleanup the interval when the component unmounts.
+    }
+  }, [])
+
   return (
     <section className={classes.promotion}>
       
