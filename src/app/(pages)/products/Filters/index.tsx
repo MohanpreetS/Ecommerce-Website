2ab700b68686d861abf76ffa316cd8c3b1eb4ad3
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2023-05-23 13:09:04 -0400

    categories page buttons

--- src/app/(pages)/products/Filters/index.tsx
+++ src/app/(pages)/products/Filters/index.tsx
@@ -27,7 +27,25 @@ const Filters = ({ categories }: { categories: Category[] }) => {
 
   return (
     <div className={classes.filters}>
-      
+      <div>
+        <h6 className={classes.title}>Product Categories</h6>
+        <div className={classes.categories}>
+          {categories.map(category => {
+            const isSelected = categoryFilters.includes(category.id)
+
+            return (
+              <Checkbox
+                key={category.id}
+                label={category.title}
+                value={category.id}
+                isSelected={isSelected}
+                onClickHandler={handleCategories}
+              />
+            )
+          })}
+        </div>
+        
+      </div>
     </div>
   )
 }
