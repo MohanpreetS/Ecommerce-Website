7babdb6f5762c7fec2e9678568dad815f9a9b080
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2023-06-01 13:09:04 -0400

    single product component finish

--- src/app/(pages)/products/[slug]/page.tsx
+++ src/app/(pages)/products/[slug]/page.tsx
@@ -40,7 +40,27 @@ export default async function Product({ params: { slug } }) {
     <>
       <ProductHero product={product} />
       {product?.enablePaywall && <PaywallBlocks productSlug={slug as string} disableTopPadding />}
-     
+      <Blocks
+        disableTopPadding
+        blocks={[
+          {
+            blockType: 'relatedProducts',
+            blockName: 'Related Product',
+            relationTo: 'products',
+            introContent: [
+              {
+                type: 'h3',
+                children: [
+                  {
+                    text: 'Related Products',
+                  },
+                ],
+              },
+            ],
+            docs: relatedProducts,
+          },
+        ]}
+      />
     </>
   )
 }
