a95dd47a1c25935772c2f4f690afaea05a58d957
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2023-06-05 13:09:04 -0400

    price component ui change

--- src/app/_components/Price/index.tsx
+++ src/app/_components/Price/index.tsx
@@ -3,8 +3,6 @@
 import React, { useEffect, useState } from 'react'
 
 import { Product } from '../../../payload/payload-types'
-import { AddToCartButton } from '../AddToCartButton'
-import { RemoveFromCartButton } from '../RemoveFromCartButton'
 
 import classes from './index.module.scss'
 
@@ -54,27 +52,14 @@ export const Price: React.FC<{
     withQuantity: priceFromJSON(priceJSON, quantity),
   }))
 
-  useEffect(() => {
-    setPrice({
-      actualPrice: priceFromJSON(priceJSON),
-      withQuantity: priceFromJSON(priceJSON, quantity),
-    })
-  }, [priceJSON, quantity])
 
   return (
     <div className={classes.actions}>
       {typeof price?.actualPrice !== 'undefined' && price?.withQuantity !== '' && (
         <div className={classes.price}>
           <p>{price?.withQuantity}</p>
-          {quantity > 1 && (
-            <small className={classes.priceBreakdown}>{`${price.actualPrice} x ${quantity}`}</small>
-          )}
         </div>
       )}
-      {button && button === 'addToCart' && (
-        <AddToCartButton product={product} appearance="default" />
-      )}
-      {button && button === 'removeFromCart' && <RemoveFromCartButton product={product} />}
     </div>
   )
 }
