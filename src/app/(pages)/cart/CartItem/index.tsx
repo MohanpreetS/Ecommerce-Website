c1f0db37afc7a2bd13413631ecf6656d9a8608e8
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2023-07-03 13:09:04 -0400

    cart item component setup

---
+++ src/app/(pages)/cart/CartItem/index.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import React, { useState } from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+import { Media } from '../../../_components/Media'
+import { Price } from '../../../_components/Price'
+import { RemoveFromCartButton } from '../../../_components/RemoveFromCartButton'
+
+import classes from './index.module.scss'
+
+const CartItem = ({ product, title, metaImage, qty, addItemToCart }) => {
+  const [quantity, setQuantity] = useState(qty)
+
+
+  return (
+    <li className={classes.item} key={title}>
+      
+    </li>
+  )
+}
+
+export default CartItem
