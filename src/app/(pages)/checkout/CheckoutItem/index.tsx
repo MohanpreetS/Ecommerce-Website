00857af0d040dea2b04d3ab77e1fa9a092ec4bb2
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2023-07-30 13:09:04 -0400

    create checkout item

---
+++ src/app/(pages)/checkout/CheckoutItem/index.tsx
@@ -0,0 +1,21 @@
+import Link from 'next/link'
+
+import { Media } from '../../../_components/Media'
+import { Price } from '../../../_components/Price'
+
+import classes from './index.module.scss'
+
+export const CheckoutItem = ({ product, title, metaImage, quantity, index }) => {
+  return (
+    <li className={classes.item} key={index}>
+      <Link href={`/products/${product.slug}`} className={classes.mediaWrapper}>
+        {!metaImage && <span>No image</span>}
+        {metaImage && typeof metaImage !== 'string' && (
+          <Media className={classes.media} imgClassName={classes.image} resource={metaImage} fill />
+        )}
+      </Link>
+
+    
+    </li>
+  )
+}
